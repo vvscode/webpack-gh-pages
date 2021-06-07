3c1eecf212183f988420029f1519ae52ef91e4a5
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-06-07 23:32:01 +0300

    Basic webpack setup

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+document.querySelector("#app").innerHTML = window.location.href;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+  },
+  entry: "./src/index.ts",
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+};
